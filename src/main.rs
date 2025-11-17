8736dbd6e192862292635215aee16f232b1225a2
Marcin Bielak <marcin.bieli@gmail.com>
2025-11-17 21:37:41 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+fn run_and_measure(cmd: &str, args: &[&str]) -> (Duration, RUsage) {
+    let start = Instant::now();
+    let end = Instant::now();
+
+    let usage = null;
+    (end - start, usage)
+}
+
 fn main() {
   todo!("TODO: impl");
 }
