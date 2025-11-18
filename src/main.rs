8469a78d24a70a30e2d3ec0d6e68203c9028987f
Marcin Bielak <marcin.bieli@gmail.com>
2025-11-18 18:56:34 +0100

    add running program in child process for real measures

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use libc::rusage;
 use std::time::Duration;
 
 use performant::measure::run_and_measure;
@@ -29,10 +30,34 @@ fn summarize_durations(durations: &[Duration]) {
 fn main() {
     let args = Args::parse();
     let mut durations = Vec::new();
+    let usage = rusage {
+        ru_utime: libc::timeval {
+            tv_sec: 0,
+            tv_usec: 0,
+        },
+        ru_stime: libc::timeval {
+            tv_sec: 0,
+            tv_usec: 0,
+        },
+        ru_maxrss: 0,
+        ru_ixrss: 0,
+        ru_idrss: 0,
+        ru_isrss: 0,
+        ru_minflt: 0,
+        ru_majflt: 0,
+        ru_nswap: 0,
+        ru_inblock: 0,
+        ru_oublock: 0,
+        ru_msgsnd: 0,
+        ru_msgrcv: 0,
+        ru_nsignals: 0,
+        ru_nvcsw: 0,
+        ru_nivcsw: 0,
+    };
 
     for i in 0..args.runs {
         println!("\n>>> Run {}/{}", i + 1, args.runs);
-        let duration = run_and_measure(
+        let (duration, usage) = run_and_measure(
             &args.program,
             &args.args.iter().map(|s| s.as_str()).collect::<Vec<_>>(),
         );
@@ -41,4 +66,5 @@ fn main() {
     }
 
     summarize_durations(&durations);
+    println!("\n===Mem Stats:\n  Max RSS (rusage): {} KB", usage.ru_maxrss);
 }
--- src/measure.rs
+++ src/measure.rs
@@ -1,9 +1,34 @@
+use libc::{getrusage, rusage, RUSAGE_CHILDREN};
+use std::process::Command;
 use std::time::Duration;
 use std::time::Instant;
 
-pub fn run_and_measure(_cmd: &str, _args: &[&str]) -> Duration {
+pub fn run_and_measure(cmd: &str, args: &[&str]) -> (Duration, rusage) {
     let start = Instant::now();
+    let mut child = Command::new(cmd)
+        .args(args)
+        .spawn()
+        .expect("Failed to start process");
+
+    let _ = child.wait().expect("Failed to wait on child");
     let end = Instant::now();
-    //TODO: implement measuring resource usage, add usage to return value
-    end - start
+
+    let usage = {
+        let mut usage = rusage {
+            ru_utime: libc::timeval {
+                tv_sec: 0,
+                tv_usec: 0,
+            },
+            ru_stime: libc::timeval {
+                tv_sec: 0,
+                tv_usec: 0,
+            },
+            ru_maxrss: 0,
+            ..unsafe { std::mem::zeroed() }
+        };
+        unsafe { getrusage(RUSAGE_CHILDREN, &mut usage) };
+        usage
+    };
+
+    (end - start, usage)
 }
